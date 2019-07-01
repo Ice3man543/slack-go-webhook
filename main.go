69344a3739cb2067d69123972f1f72360f0e7f23
Ice3man543 <nizamulrana@gmail.com>
2019-07-01 20:52:01 +0530

    Added http client

--- main.go
+++ main.go
@@ -2,24 +2,29 @@ package slack
 
 import (
 	"bytes"
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
+// Field is a field sent to slack
 type Field struct {
 	Title string `json:"title"`
 	Value string `json:"value"`
 	Short bool   `json:"short"`
 }
 
+// Action is an action object of slack
 type Action struct {
 	Type  string `json:"type"`
 	Text  string `json:"text"`
-	Url   string `json:"url"`
+	URL   string `json:"url"`
 	Style string `json:"style"`
 }
 
+// Attachment is a slack attachment
 type Attachment struct {
 	Fallback     *string   `json:"fallback"`
 	Color        *string   `json:"color"`
@@ -30,7 +35,7 @@ type Attachment struct {
 	Title        *string   `json:"title"`
 	TitleLink    *string   `json:"title_link"`
 	Text         *string   `json:"text"`
-	ImageUrl     *string   `json:"image_url"`
+	ImageURL     *string   `json:"image_url"`
 	Fields       []*Field  `json:"fields"`
 	Footer       *string   `json:"footer"`
 	FooterIcon   *string   `json:"footer_icon"`
@@ -38,13 +43,14 @@ type Attachment struct {
 	MarkdownIn   *[]string `json:"mrkdwn_in"`
 	Actions      []*Action `json:"actions"`
 	CallbackID   *string   `json:"callback_id"`
-	ThumbnailUrl *string   `json:"thumb_url"`
+	ThumbnailURL *string   `json:"thumb_url"`
 }
 
+// Payload is the payload sent to the slack
 type Payload struct {
 	Parse       string       `json:"parse,omitempty"`
 	Username    string       `json:"username,omitempty"`
-	IconUrl     string       `json:"icon_url,omitempty"`
+	IconURL     string       `json:"icon_url,omitempty"`
 	IconEmoji   string       `json:"icon_emoji,omitempty"`
 	Channel     string       `json:"channel,omitempty"`
 	Text        string       `json:"text,omitempty"`
@@ -55,18 +61,44 @@ type Payload struct {
 	Markdown    bool         `json:"mrkdwn,omitempty"`
 }
 
+// Agent is an agent for sending slack webhook messages
+type Agent struct {
+	client *http.Client
+}
+
+// NewAgent creates a new slack webhook sending agent
+func NewAgent() *Agent {
+	return &Agent{
+		client: &http.Client{
+			Transport: &http.Transport{
+				MaxIdleConns:        1024,
+				MaxIdleConnsPerHost: 1024,
+				TLSClientConfig: &tls.Config{
+					InsecureSkipVerify: true,
+				},
+			},
+			Timeout: time.Duration(30) * time.Second,
+			CheckRedirect: func(req *http.Request, resp []*http.Request) error {
+				return nil
+			},
+		},
+	}
+}
+
+// AddField adds a slack webhook field
 func (attachment *Attachment) AddField(field Field) *Attachment {
 	attachment.Fields = append(attachment.Fields, &field)
 	return attachment
 }
 
+// AddAction adds a slack webhook action
 func (attachment *Attachment) AddAction(action Action) *Attachment {
 	attachment.Actions = append(attachment.Actions, &action)
 	return attachment
 }
 
 // Send sends a request to a webhook url
-func Send(webhookURL string, payload Payload) error {
+func (a *Agent) Send(webhookURL string, payload Payload) error {
 	buf := new(bytes.Buffer)
 
 	err := json.NewEncoder(buf).Encode(payload)
@@ -74,7 +106,7 @@ func Send(webhookURL string, payload Payload) error {
 		return err
 	}
 
-	resp, err := http.Post(webhookURL, "application/json", buf)
+	resp, err := a.client.Post(webhookURL, "application/json", buf)
 	if err != nil {
 		return err
 	}
